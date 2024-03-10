fb8c6fef1eba133efd051af94fdcfd14c1dfd35c
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-03-10 19:25:38 +0700

    Remove game start route

--- routes/game.js
+++ routes/game.js
@@ -3,9 +3,7 @@ const game_controller = require("../controller/game-controller");
 
 const router = express.Router();
 
-router.get("/:name", game_controller.game_get);
-
-router.get("/:name/start", game_controller.game_start);
+router.get("/:name", game_controller.game_start);
 
 router.post("/", game_controller.game_post);
 
