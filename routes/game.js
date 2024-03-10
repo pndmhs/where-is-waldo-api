4bcfa2314c3b0b8d3fcbad4cb434c24bd3125fc7
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-03-10 22:46:39 +0700

    Add targets router

--- routes/game.js
+++ routes/game.js
@@ -7,4 +7,6 @@ router.get("/:name", game_controller.game_start);
 
 router.post("/", game_controller.game_post);
 
+router.post("/:name/targets", game_controller.check_coordinate);
+
 module.exports = router;
