c379efbd9fde7f07f5100ef14a525253bbea451e
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-03-10 14:42:32 +0700

    Add game post controller

--- controller/game-controller.js
+++ controller/game-controller.js
@@ -1,5 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
+const { body, validationResult } = require("express-validator");
+const Game = require("../models/game");
 require("dotenv").config();
 
 exports.game_start = asyncHandler(async (req, res, next) => {
@@ -11,6 +13,32 @@ exports.game_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Target data GET");
 });
 
-exports.game_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Target data POST");
-});
+exports.game_post = [
+  body("name", "Name must not be empty").trim().isLength({ min: 3 }).escape(),
+  body("targets.*.character").trim().isLength({ min: 3 }).escape(),
+  body("targets.*.min_x").isNumeric(),
+  body("targets.*.max_x").isNumeric(),
+  body("targets.*.min_y").isNumeric(),
+  body("targets.*.max_y").isNumeric(),
+
+  asyncHandler(async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const game = new Game({
+        name: req.body.name,
+        targets: req.body.targets,
+      });
+
+      await game.save();
+      res.status(201).json(game);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+    }
+  }),
+];
