8501e13bf7b5e847e2a01f4456e13749d5affc34
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-03-10 14:42:15 +0700

    Add connection to the mongodb database

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ const scoreRouter = require("./routes/score");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+const mongoDB = process.env.MONGODB_URI;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger("dev"));
