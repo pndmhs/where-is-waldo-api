07f238860006475ea5891113d7925796aa57ccaa
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-03-10 12:56:38 +0700

    Add game start route

--- routes/game.js
+++ routes/game.js
@@ -3,7 +3,9 @@ const game_controller = require("../controller/game-controller");
 
 const router = express.Router();
 
-router.get("/", game_controller.game_get);
+router.get("/:name", game_controller.game_get);
+
+router.get("/:name/start", game_controller.game_start);
 
 router.post("/", game_controller.game_post);
 
