998657ef2f1e208e3b56a4d879d530b297876a33
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-03-10 12:56:29 +0700

    Add game start controller

--- controller/game-controller.js
+++ controller/game-controller.js
@@ -1,4 +1,11 @@
 const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+exports.game_start = asyncHandler(async (req, res, next) => {
+  const token = jwt.sign({ date: Date.now() }, process.env.TOKEN_SECRET);
+  res.json({ token });
+});
 
 exports.game_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Target data GET");
