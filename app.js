3165e2fe170000422b96f30482d77d5f1175afad
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-03-12 17:16:45 +0700

    Add cors middleware

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
+const cors = require("cors");
 
 require("dotenv").config();
 
@@ -22,6 +23,7 @@ async function main() {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger("dev"));
+app.use(cors());
 
 app.use("/game", gameRouter);
 app.use("/scores", scoreRouter);
