6a85951ee99f71010c4728c9f50d4237c8804fe3
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-03-14 22:13:59 +0700

    Add game_name param to score get route

--- routes/score.js
+++ routes/score.js
@@ -3,7 +3,7 @@ const score_controller = require("../controller/score-controller");
 
 const router = express.Router();
 
-router.get("/", score_controller.score_get);
+router.get("/:game_name", score_controller.score_get);
 
 router.post("/", score_controller.score_post);
 
