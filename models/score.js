a71bded4347fd94e9e6f99d118d2ee9ed3de5961
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-03-14 11:34:27 +0700

    Change schema fields

--- models/score.js
+++ models/score.js
@@ -3,9 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ScoreSchema = new Schema({
-  name: { type: String, require: true, minLength: 3 },
-  start_time: { type: Date, require: true },
-  end_time: { type: Date, require: true },
+  username: { type: String, require: true, minLength: 3 },
+  score: { type: String, require: true },
   date: { type: Date, require: true },
   game: { type: Schema.ObjectId, ref: "Game" },
 });
